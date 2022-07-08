7d194be7e8ca1ca64eec75098a3ab56e80831200
ahndaeun <dean034@naver.com>
2022-07-08 10:41:47 +0900

    7.8 탄창 예제

--- src/kr/org/loop/Loop4_ex1.java
+++ src/kr/org/loop/Loop4_ex1.java
@@ -1,23 +1,61 @@
 package kr.org.loop;
 
+import java.util.Scanner;
+
 public class Loop4_ex1 {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
 		//분과 초 입력받음
 		// ex) (분)1, (초)15
+		System.out.println("분 입력");
+		int min = sc.nextInt();
 		
+		System.out.println("초 입력");
+		int sec = sc.nextInt();
+		
+		
+		boolean flag= false;
 		
 		
 		for (int m=0; m<60; m++) {
-			
+			//여기에 if를 쓰면 초가 선언이 안된 상태라 분만 실행됨
 			for (int s =0; s<60; s++) {
 				
 				System.out.println(m+" 분 : "+s+" 초 ");
+			//초는 선언된 중괄호 내에서만 사용가능	, 이중 for문 실행하기위해서 안쪽 for문에 작성해야 실행
+				if (min==m&&sec==s) {
+					flag=!flag; //!반대값 ex) !true =false 
+					break; // if 
+					
+				} // 선언이 종료되는 두가지: break; false가 될때
 				
-				}//
+				}
 			
+			if(flag) {
+				break; // 바깥쪽 for문도 종료시키고 싶을때
+			}
+				//if문이 들어올 수 있지만 초를 인식하지 못함
 			} 
 			
 }
 }
+
+//System.out.println("+++++++++++++++++++++++++++++++");
+//
+//for (int m=0; m<60; m++) {
+//	
+//	for (int s =0; s<60; s++) {
+//		
+//		System.out.println(m+" 분 : "+s+" 초 ");
+//		
+//		if (min==m&&sec==s) {
+//			flag=!flag;  
+//			m=60; 
+//			break;  
+//			
+//		} 
+//	}
+//}	
+
+	
---
+++ src/kr/org/loop/Loop4_ex2.java
@@ -0,0 +1,93 @@
+package kr.org.loop;
+
+import java.util.Scanner;
+
+public class Loop4_ex2 {
+
+	public static void main(String[] args) {
+	
+		// FPS
+		// 총알:
+		// 탄창:30발 3개
+		// 1. 단발 30발 2. 점사 10발
+		
+		Scanner sc= new Scanner( System.in );
+		
+	
+		for( int b=0; b<3; b++ ) {
+					
+			System.out.println(" 1. 단발 2. 점사 ");
+
+			int g= sc.nextInt();
+			
+		if(g==1) {
+		
+				for(int c=0; c<30; c++) {
+					
+					System.out.print("탕"); // ln(enter, 줄바꿈) ln x 이어쓰기 
+					
+			
+		}
+			
+		}else if (g!=1){
+			
+			for(int c=0; c<10; c++) {
+				
+				System.out.print("탕탕탕");
+
+		}
+
+	}System.out.println(""); // 
+
+
+}
+}
+}
+
+//-------------------------------------------
+//드레그 +alt + 화살표 복사 옮기기
+
+
+//// (int mag=0; mag <3; mag++){
+//system.out.println("1.단발, 2점사");
+//int g= sc.nextInt();
+////
+//IF (select==1){
+//	for (int b=0; b<30; b++){
+//system.out.println(탄창: "mag+"총알:"+
+//}
+//else}
+// for (int b=0; b<30' b++){
+//system.out.println(탄창: "mag+"총알:"+
+//}
+
+
+
+//--------------------------------------
+//(int mag=0; mag <3; mag++){
+//System.out.println("1.단발, 2점사");
+//int select= sc.nextInt();
+//int count=0;
+//String sound= "탕탕탕";
+//if(select==1){
+//	count=30; //1이면 30 1아니면 30x
+//	sound= "탕";
+//}
+//for (int b=0; b<10; b++){
+//System.out.println(sound);
+//}
+
+		
+//------------------------------------
+//(int mag=0; mag <3; mag++){
+//System.out.println("1.단발, 2점사");
+//int select= sc.nextInt();
+//int inc=3;
+//String sound= "탕탕탕";
+//if(select==1){
+//	inc=1; 
+//	sound= "탕";
+//}
+//for(int b=0; b<10; b=b+inc){
+//System.out.println(sound);
+//}
