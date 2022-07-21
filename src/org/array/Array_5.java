18ca436d6a5ce1a598d0d72a7ddff2726e796dc1
ahndaeun <dean034@naver.com>
2022-07-21 17:02:46 +0900

    7월21일

--- src/org/array/Array_5.java
+++ src/org/array/Array_5.java
@@ -4,24 +4,31 @@ public class Array_5 {
 
 	public static void main(String[] args) {
 		
-		for (int i=0; i<nums1.length; i++) {
-			
-			nums3[i] =nums1[i];
-		}
+
+
+		int [] nums1 = {1,2,3};
+		System.out.println(nums1.length);
+		//얕은 복사
+		int [] nums2 = nums1;
+		System.out.println(nums2.length);
+		
+		//깊은 복사
 		//새로운배열을 만들고 값을 복사하는 건 깊은 복사
 		//독립적인 데이터
+		int [] nums3 = new int[3];
+		for(int i=0;i<nums1.length;i++) {
+			nums3[i]=nums1[i];
+		}
 		nums2[0]=9;
-		\
-		
 		System.out.println(nums1[0]);
 		
-		nums3[0]=100;
-		System.out.println(nums1[0]);
-		//깊은 복사
 		
-		nums3= new int [5];
+		System.out.println(nums3.length);
+		nums3 = new int [5];
 		System.out.println(nums3[0]);
 		
+		
+
 	}
 
 }
